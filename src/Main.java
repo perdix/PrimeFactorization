569d36ec15e872b8f36a3c30a291c0daeb37d07c
Paul Opitz <paul.opitz@outlook.com>
2025-01-27 20:44:21 +0100

    Updates

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,36 @@
-import com.sun.tools.jconsole.JConsoleContext;
-
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        // Create a queue
-        Queue<Integer> queue = new LinkedList<>();
+        final int LIMIT = 200000;
 
-        // Fill the queue with numbers from 1 to 100
-        for (int i = 1; i <= 300000; i++) {
-            queue.add(i);
+        // Create a list and fill it with numbers
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= LIMIT; i++) {
+            list.add(i);
         }
+
+
 
         long startTime = System.nanoTime();
 
         // Print the prime of all numbers in the queue
-        while (!queue.isEmpty()) {
-            int n = queue.poll();
+        for (Integer n : list) {
             printPrimeFactors(n);
         }
 
         long endTime = System.nanoTime();
 
+        // Print the overall time taken
         System.out.println("Time taken: " + (endTime - startTime) / 1000000 + "ms");
 
     }
 
     // Helper function to print prime factors of a number
-    public static void printPrimeFactors(int n){
+    public static void printPrimeFactors(Integer n){
         System.out.print("Prime factors of " + n + " are: ");
         for (int i = 2; i <= n; i++) {
             while (n % i == 0) {
